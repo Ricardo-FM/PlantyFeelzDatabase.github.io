6d415bfd19122640c7da532b5ca3b2ac253a7200
Christopher Nguyen <christophernguyeen@gmail.com>
2021-02-03 22:20:56 -0800

    update file

--- models/plant.js
+++ models/plant.js
@@ -1,18 +1,38 @@
-'use strict';
+'use strict'
 
 module.exports = (sequelize, DataTypes) => {
-    const Plants = sequelize.define('Plants', {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
-    });
-    Plants.associate = models => {
-        Plants.belongsTo(models.Plants, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    }
-    return Plants;
+  const Plant = sequelize.define('Plant', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    destination: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    methodOfTransport: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    // The password cannot be null
+    arrivalDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  })
+  Plant.associate = (models) => {
+    // associations can be defined here
+    Plant.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false,
+      },
+    })
+  }
+  return Plant
 }
