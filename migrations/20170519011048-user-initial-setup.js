a7420366f67a0b6999be7755884de51cb22a6931
Van Taymany <vtaymany@gmail.com>
2021-02-07 00:07:35 -0800

    updates database

--- migrations/20170519011048-user-initial-setup.js
+++ migrations/20170519011048-user-initial-setup.js
@@ -1,41 +1,41 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-      return queryInterface.createTable('Users', {
+    return queryInterface.createTable('Users', {
       id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
         autoIncrement: true,
-        allowNull: false
+        allowNull: false,
       },
       username: {
         type: Sequelize.STRING,
         allowNull: false,
         validate: {
-          len: [6]
-        }
+          len: [6],
+        },
       },
       email: {
         type: Sequelize.STRING,
         allowNull: false,
         validate: {
-          isEmail: true
-        }
+          isEmail: true,
+        },
       },
       password: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
       },
       createdAt: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
-        type: Sequelize.DATE
-      }
-    });
+        type: Sequelize.DATE,
+      },
+    })
   },
   down: function (queryInterface, Sequelize) {
-      return queryInterface.dropTable('Users');
-  }
-};
+    return queryInterface.dropTable('Users')
+  },
+}
